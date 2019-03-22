45db36381bc9578f534cc9340d32bd88542260de
Francisco Belmonte Ruiz <francisco.belmonte.ruiz@gmail.com>
2019-03-22 16:41:58 +0100

    Cors

--- 01-server/dist/index.js
+++ 01-server/dist/index.js
@@ -6,9 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("./classes/server"));
 const router_1 = __importDefault(require("./routes/router"));
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const server = new server_1.default();
 server.app.use(bodyParser.urlencoded({ extended: true }));
 server.app.use(bodyParser.json());
+server.app.use(cors({ origin: true, credentials: true }));
 server.app.use('/', router_1.default);
 server.start(() => {
     console.log(`Server running in ${server.port}`);
--- 01-server/index.ts
+++ 01-server/index.ts
@@ -1,10 +1,12 @@
 import Server from "./classes/server";
 import router from "./routes/router";
 import bodyParser = require("body-parser");
+import cors = require("cors");
 
 const server = new Server();
 server.app.use(bodyParser.urlencoded({extended: true}));
 server.app.use(bodyParser.json());
+server.app.use(cors({origin: true, credentials: true}));
 server.app.use('/', router);
 
 server.start(() => {

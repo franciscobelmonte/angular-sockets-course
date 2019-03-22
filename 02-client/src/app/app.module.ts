81f0082cf2a9e0e31d120935df8002578aa1b796
Francisco Belmonte Ruiz <francisco.belmonte.ruiz@gmail.com>
2019-03-22 17:24:15 +0100

    Angular Socket client App using ngx-socket-io

---
+++ 02-client/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
+import { AppComponent } from './app.component';
+
+import { environment } from '../environments/environment';
+
+const config: SocketIoConfig = {
+  url: environment.wsUrl,
+  options: {}
+};
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    SocketIoModule.forRoot(config)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

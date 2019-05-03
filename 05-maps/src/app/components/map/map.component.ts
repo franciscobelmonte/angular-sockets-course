8f0679a276b2066db2ae7a5e5ce02170eadc569f
Francisco Belmonte Ruiz <francisco.belmonte.ruiz@gmail.com>
2019-05-03 15:32:22 +0200

    Add markers to map

--- 05-maps/src/app/components/map/map.component.ts
+++ 05-maps/src/app/components/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Place } from '../../models/place';
 
 @Component({
   selector: 'app-map',
@@ -8,6 +9,25 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 export class MapComponent implements OnInit {
   @ViewChild('map') mapElement: ElementRef;
   map: google.maps.Map;
+  markers: google.maps.Marker[] = [];
+
+  places: Place[] = [
+    {
+      name: 'Udemy',
+      lat: 37.784679,
+      lng: -122.395936
+    },
+    {
+      name: 'Bahía de San Francisco',
+      lat: 37.798933,
+      lng: -122.377732
+    },
+    {
+      name: 'The Palace Hotel',
+      lat: 37.788578,
+      lng: -122.401745
+    }
+  ];
 
   constructor() { }
 
@@ -24,6 +44,23 @@ export class MapComponent implements OnInit {
     };
 
     this.map = new google.maps.Map(this.mapElement.nativeElement, options);
+
+    for (const place of this.places) {
+      this.addMarker(place);
+    }
+  }
+
+  addMarker (place: Place) {
+    const coordinates = new google.maps.LatLng(place.lat, place.lng);
+
+    const marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: coordinates,
+      draggable: true
+    });
+
+    this.markers.push(marker);
   }
 
 }

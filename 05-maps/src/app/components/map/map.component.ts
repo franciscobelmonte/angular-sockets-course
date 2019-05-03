651f16418a88e3a4b1e639b637a47e579e1bfda9
Francisco Belmonte Ruiz <francisco.belmonte.ruiz@gmail.com>
2019-05-03 15:24:59 +0200

    Load Google Maps using types/googlemaps

--- 05-maps/src/app/components/map/map.component.ts
+++ 05-maps/src/app/components/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-map',
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
+  @ViewChild('map') mapElement: ElementRef;
+  map: google.maps.Map;
 
   constructor() { }
 
   ngOnInit() {
+    this.loadMap();
+  }
+
+  loadMap () {
+    const coordinates = new google.maps.LatLng(37.784679, -122.395936);
+    const options: google.maps.MapOptions = {
+      center: coordinates,
+      zoom: 13,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, options);
   }
 
 }
